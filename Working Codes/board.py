15ca5006fadeaf67b960b3657e9e91b401dd44b0
Asritha  Bhimavarapu <20wh1a0510@bvrithyderabad.edu.in>
2021-10-22 18:02:17 +0000

    Update board.py

--- Working Codes/board.py
+++ Working Codes/board.py
@@ -2,15 +2,13 @@ class Board():
     def __init__(self, size):
         self.size = size
         self.createBoard()
-        #self.prob = prob
+        
 
     def createBoard(self):
         self.board = []
         for row in range(self.size[0]):
             row = []
             for col in range(self.size[1]):
-                #hasBomb = random() < self.prob
-                #piece = Piece(hasBomb)
                 piece = None
                 row.append(piece)
             self.board.append(row)
