281cd861e3432b7d666fda61471ac9fa4e8811bd
Chithra Bhanu Aalla <20wh1a6614@bvrithyderabad.edu.in>
2021-10-22 08:55:20 +0000

    Replace game__1_.py

--- Code for mine sweeper/game__1_.py
+++ Code for mine sweeper/game__1_.py
@@ -1,41 +1,35 @@
 import pygame
 import os
-
 class Game():
     def __init__(self, board, screenSize):
         self.board = board
         self.screenSize = screenSize
-        self.pieceSize =  self.screenSize[0] //  self.board.getSize()[1],  self.screenSize[1] //  self.board.getSize()[0]
-        self.loadImages()
-    
+        self.gameScreen = self.getGameScreen()
+
     def run(self):
         pygame.init()
         self.screen = pygame.display.set_mode(self.screenSize)
         running = True
         while running:
             for event in pygame.event.get():
-                if (event.type == pygame.QUIT):
+                if event.type == pygame.QUIT:
                     running = False
-            self.draw()
-            pygame.display.flip()
+            self.graphics()
         pygame.quit()
 
-    def draw(self):
-        topLeft = (0, 0)
-        for row in range(self.board.getSize()[0]):
-            for col in range(self.board.getSize()[1]):
-                image = self.images["empty_block"]
+    def graphics(self):
+        gameBoardStart_col = self.screenSize[1] // 0.1
+        topLeft = (0, gameBoardStart_col)
+        for row in range(self.board.getSizeOfBoard()[0]):
+            for col in range(self.board.getSizeOfBoard()[1]):
+                image = pygame.image.load("D:\Chithra\Python\WISE\Project Minesweeper\Slow Code\empty_block.png")
                 self.screen.blit(image, topLeft)
-                topLeft = topLeft[0] + self.pieceSize[0], topLeft[1]
-            topLeft = 0, topLeft[1] + self.pieceSize[1]
+                topLeft = topLeft[0] + self.getGridSize()[0], topLeft[1]
+            topLeft = 0, topLeft[1] + self.getGridSize()[1]
+        
+    def getGridSize(self):
+        self.gridSize = (self.gameScreen[0] // self.board.getSizeOfBoard()[0], self.gameScreen[1] // self.board.getSizeOfBoard()[1])
+        return self.gridSize
 
-    def loadImages(self):
-        self.images = {}
-        for filename in os.listdir("Images"):
-            if (not filename.endswith(".png")):
-                continue
-            image = pygame.image.load(r"Images/" + filename)
-            image = pygame.transform.scale(image, self.pieceSize)
-            self.images[filename.split(".")[0]] = image
-
-    
+    def getGameScreen(self):
+        return (self.screenSize[0], self.screenSize[1] // 0.9)
