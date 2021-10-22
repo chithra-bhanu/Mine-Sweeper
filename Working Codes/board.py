47ad50f2ae7aea61924c6d237fc3cb3f447cd9d6
Asritha  Bhimavarapu <20wh1a0510@bvrithyderabad.edu.in>
2021-10-22 17:58:28 +0000

    Upload New File

---
+++ Working Codes/board.py
@@ -0,0 +1,28 @@
+from piece import Piece
+import random
+
+class Board():
+    def __init__(self, size):
+        self.size = size
+        self.createBoard()
+        #self.prob = prob
+
+    def createBoard(self):
+        self.board = []
+        for row in range(self.size[0]):
+            row = []
+            for col in range(self.size[1]):
+                #hasBomb = random() < self.prob
+                #piece = Piece(hasBomb)
+                piece = None
+                row.append(piece)
+            self.board.append(row)
+
+    def board_size(self):
+        return self.size
+        
+
+
+
+
+
