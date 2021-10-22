99c69b374f8a2ae01a309fdfaff143a99f2c3384
Chithra Bhanu Aalla <20wh1a6614@bvrithyderabad.edu.in>
2021-10-22 08:54:33 +0000

    Replace board__1_.py

--- Code for mine sweeper/board__1_.py
+++ Code for mine sweeper/board__1_.py
@@ -1,16 +1,18 @@
 class Board():
-    def __init__(self, size):
-        self.size = size
-        self.setBoard()
+    def __init__(self, sizeOfBoard):
+        self.sizeOfBoard = sizeOfBoard
+        self.createBoard()
 
-    def setBoard(self):
-        self.board = []
-        for row in range(self.size[0]):
+    def createBoard(self):
+        self.gameBoard = []
+        for row in range(self.sizeOfBoard[0]):
             row = []
-            for col in range(self.size[1]):
-                piece = None
-                row.append(piece)
-            self.board.append(row)
+            for col in range(self.sizeOfBoard[1]):
+                grid = None
+                row.append(grid)
+            self.gameBoard.append(row)
 
-    def getSize(self):
-        return self.size        
+    def getSizeOfBoard(self):
+        return self.sizeOfBoard
+
+
