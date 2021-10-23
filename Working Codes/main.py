8e3d6151c1763341651e3d7d96557aade4e7ed08
Asritha  Bhimavarapu <20wh1a0510@bvrithyderabad.edu.in>
2021-10-23 00:35:51 +0000

    Updated till placing mines

--- Working Codes/main.py
+++ Working Codes/main.py
@@ -1,8 +1,8 @@
 from game import Game
 from board import Board
 size = (9, 9)
-#prob = 0.5
-board  = Board(size)
-screen_size = (800, 500)
+prob = 0.2
+board  = Board(size, prob)
+screen_size = (500, 400)
 game = Game(board, screen_size)
 game.runBoard()
